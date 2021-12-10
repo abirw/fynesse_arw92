4558fdd032a53cf3d3db26506b1121c33f5fdbac
abirw <abicarver13@gmail.com>
2021-12-10 11:27:32 +0000

    updates

--- fynesse/access.py
+++ fynesse/access.py
@@ -14,12 +14,6 @@ import osmnx as ox
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
-def data():
-    """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
-
-
-
 # --------------------------------------------------------------------------------------------
 # SETTING UP THE DATABASE
 # --------------------------------------------------------------------------------------------
--- fynesse/address.py
+++ fynesse/address.py
@@ -19,6 +19,10 @@ import math
 from . import access
 from . import assess
 
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 def get_properties_bounding_boxw_dist(lat,lon,dist,pt,date):
   up = dist/111
   across = dist/(111*math.cos(math.radians(lat)))
@@ -73,3 +77,75 @@ def get_properties_bounding_dist(lat,lon,dist,pt,date):
   cols= [column[0] for column in cur.description] 
   rows = cur.fetchall()
   return rows, cols
+
+def get_distance_to_point(lat, lon, pdf):
+  pdf["lat"] = pd.to_numeric(pdf["lat"])
+  pdf["lon"] = pd.to_numeric(pdf["lon"])
+  pdf["distance"] = pdf.apply(lambda row : assess.get_distance(lat, lon, row["lat"], row["lon"]), axis=1)
+  return pdf
+
+def get_weights(weight_cols, w, d):
+  date = pd.to_datetime(d)
+  weight1 = pd.DataFrame(data=w, columns=weight_cols)
+  weight1["date_diff"] = weight1["date_int"]-(date.value/10**9)
+  weight1["date_diff"] = 1 - weight1["date_diff"].abs()/weight1["date_diff"].abs().max()
+  weight1["distance_w"] = 1 - weight1["distance"]/weight1["distance"].max()
+  weight1["combined"] = weight1["date_diff"]*weight1["distance_w"]
+  weight1["combined"] = weight1["combined"]**0.5
+  return weight1[["date_diff", "distance_w", "combined"]]
+
+def get_prediction_data(lat, lon, date, kinds, sizes, dist):
+  pcs1 = pd.DataFrame(data=[["XX", lat, lon]], columns=["postcode", "lat", "lon"])
+  to_predict, _ = assess.get_features_from_list(pcs1, lat, lon, kinds, sizes,dist)
+  date = pd.to_datetime("2018-01-01")
+  to_predict["date_int"] = pd.to_datetime(date).value/10**9
+  to_predict["lat"]=lat
+  to_predict["lon"]=lon
+  return to_predict
+
+
+def make_prediction(lat, lon, date, pt):
+  dist, kinds, sizes = (3, ["schools", "public_transport","parks_and_rec", "services", "rural"],[1,1,1,1,1])
+  props = assess.get_props_with_features(lat, lon, date, pt, dist, kinds, sizes)
+  props = address.get_distance_to_point(lat,lon,props)
+
+
+  features = list(props.columns)
+  for x in ['price', 'date_int', 'lat', 'lon']:
+    features.remove(x)
+
+  weight_cols = ['date_int', 'distance']
+  x = b_feat[features].to_numpy()
+  y = b_feat["price"].to_numpy()
+  w = b_feat[weight_cols].to_numpy()
+  
+  ss = StandardScaler()
+  x = ss.fit_transform(x)
+
+  pca = PCA(0.9)
+  prin_comp = pca.fit_transform(x)
+
+  new_props = pd.DataFrame(data = prin_comp, columns=["component_"+str(x) for x in range(prin_comp.shape[1])])
+  columns = new_props.columns
+
+  new_props = pd.concat([new_props, props[["price"]]], axis=1)
+
+  weight1 = get_weights(weight_cols, w)
+
+  to_pred = get_prediction_data(lat, lon,date, kinds, sizes, dist)
+
+  x1= new_props[columns].to_numpy()
+
+  m_poisson_comb = PoissonRegressor().fit(x1,y, sample_weight=weight1["combined"].to_numpy())
+
+
+  xtopred = to_predict[features].to_numpy()
+
+
+  xtopred = ss.transform(xtopred)
+  to_pred = pca.transform(xtopred)
+
+  pred_price_comb = m_poisson_comb.predict(to_pred)
+  comb_pred = pred_price_comb[0]
+
+  print("prediction: £",comb_pred)
